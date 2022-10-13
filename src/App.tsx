2e05bc950c02364a667dd80ad85bbf21cf47b8df
Denis <lapkod51@gmail.com>
2022-10-13 20:05:55 +0300

    React Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react'
 
-function App() {
-  return <div className="App"></div>;
+// React Element with JSX
+// const title = <h1>Hello React.js </h1>
+
+// const title = React.createElement("h1",null,"React")
+
+// const list = (
+//   <ul>
+//     <li>list item 1</li>
+//     <li>list item 2</li>
+//     <li>list item 3</li>
+//   </ul>
+// )
+
+// React Component 
+const Title = () => <h1>Hello React component</h1>
+
+const List = () => {
+  let a = 10
+  return (
+  <ul>
+    <li>list item {a + 1}</li>
+    <li>list item 2</li>
+    <li>list item 3</li>
+  </ul>
+
+  )
 }
 
-export default App;
+function App() {
+    return <div className="App"><Title /> <List/> {10 + 5}</div>
+}
+
+export default App
