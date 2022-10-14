39a6dc4e71ec5d7eff45f5273df3839d0d71f55e
Denis <lapkod51@gmail.com>
2022-10-14 19:51:30 +0300

    added header.tsx main.tsx and footer.tsx

---
+++ src/container/App/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
